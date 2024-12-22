669c5d8998821ed2a31befe83d47d2f85b38400d
Ahmad Ryan Permana <ahmadryanpermana@gmail.com>
2024-12-22 22:36:07 +0700

    Time: 483 ms (13.75%), Space: 66 MB (97.87%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.py
+++ 0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -3,7 +3,7 @@ import pandas as pd
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
     values = list(employee['salary'].sort_values(ascending=False))
     result = [y for x,y in enumerate(values) if y not in values[:x]]
-    if N > len(result):
+    if N > len(result) or N <= 0:
         return pd.DataFrame({'getNthHighestSalary({})'.format(N):[None]})
     else:
         return pd.DataFrame({'getNthHighestSalary({})'.format(N):[result[N-1]]})
