5de6c122872c8f750b7d8cb564e75c38ed8d94f3
Ahmad Ryan Permana <ahmadryanpermana@gmail.com>
2024-12-25 13:08:07 +0700

    Update for sql queries.

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,10 +1,4 @@
 # Write your MySQL query statement below
 
-SELECT 
-    score
-    CASE
-        WHEN COUNT(*) > 1 THEN 'Duplicated'
-        ELSE 'NOPE'
-    END AS 'RANK'
-FROM scores 
-ORDER BY score DESC;
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) AS rank
+FROM Scores
