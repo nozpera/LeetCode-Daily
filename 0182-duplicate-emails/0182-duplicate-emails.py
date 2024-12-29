c832533f733f5cd966a33d758d753798fc67742a
Ahmad Ryan Permana <ahmadryanpermana@gmail.com>
2024-12-29 22:00:51 +0700

    Time: 445 ms (26.27%), Space: 66.7 MB (82.2%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.py
+++ 0182-duplicate-emails/0182-duplicate-emails.py
@@ -4,4 +4,3 @@ def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
     if person['email'].duplicated().any():
         return person[['email']][(person['email'].duplicated(keep=False)) & (~person['email'].duplicated())]
     return person[['email']][person['email'].duplicated()]
-    
