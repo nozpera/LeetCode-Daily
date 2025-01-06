78b3c9729562bcc854f8e1acf96ece6d82c69e22
Ahmad Ryan Permana <ahmadryanpermana@gmail.com>
2025-01-06 13:57:13 +0700

    remove empty cells

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
@@ -3,6 +3,3 @@ import pandas as pd
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
     person.sort_values('id', inplace=True)
     person.drop(person.loc[person['email'].duplicated()].index, inplace=True)
-
-    
-    
