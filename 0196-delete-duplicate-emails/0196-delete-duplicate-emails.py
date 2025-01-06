4dbd717dd239a670024dc51e9d7845bfa62462df
Ahmad Ryan Permana <ahmadryanpermana@gmail.com>
2025-01-06 16:05:23 +0700

    Time: 672 ms (5.51%), Space: 67.4 MB (39.81%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
@@ -3,3 +3,6 @@ import pandas as pd
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
     person.sort_values('id', inplace=True)
     person.drop(person.loc[person['email'].duplicated()].index, inplace=True)
+
+    
+    
