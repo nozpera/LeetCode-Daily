d26c780c5537808b042606f8620ce11b0c7a7fad
Ahmad Ryan Permana <ahmadryanpermana@gmail.com>
2025-01-07 07:47:08 +0700

    Time: 577 ms (15.09%), Space: 67.4 MB (67.07%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+    weather = weather.sort_values('recordDate')
+    return weather[['id']][(weather.recordDate - weather.recordDate.shift(1) == pd.Timedelta(days=1)) & (weather.temperature > weather.temperature.shift(1))]
+
+    
