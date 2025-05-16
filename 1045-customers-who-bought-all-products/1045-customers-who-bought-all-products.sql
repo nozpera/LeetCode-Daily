2330da28131a784d80cc7bfbd0df940be4270862
Ahmad Ryan Permana <ahmadryanpermana@gmail.com>
2025-05-16 07:50:59 +0700

    Time: 549 ms (81.97%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,6 +1,10 @@
 # Write your MySQL query statement below
+WITH nodupl AS (SELECT *,
+ROW_NUMBER() OVER (PARTITION BY customer_id, product_key) AS rn
+FROM customer)
 SELECT DISTINCT customer_id
-FROM (SELECT customer_id,
-COUNT(*) OVER (PARTITION BY customer_id) AS cn
-FROM customer) AS sq
+FROM (
+    SELECT *,
+    COUNT(*) OVER (PARTITION BY customer_id) AS cn FROM nodupl WHERE rn = 1
+) AS subq
 WHERE cn = (SELECT COUNT(*) FROM product)
