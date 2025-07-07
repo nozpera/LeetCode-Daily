de4b9263d8821ebacac7aaaeaa67854bace31756
Ahmad Ryan Permana <ahmadryanpermana@gmail.com>
2025-07-07 08:03:01 +0700

    Time: 509 ms (58.83%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -2,5 +2,5 @@
 WITH merged AS (SELECT p.project_id, e.employee_id, e.name, e.experience_years
 FROM project AS p
 INNER JOIN employee AS e ON (e.employee_id = p.employee_id))
-SELECT project_id, AVG(experience_years) AS average_years
+SELECT project_id, ROUND(AVG(experience_years), 2) AS average_years
 FROM merged GROUP BY 1
